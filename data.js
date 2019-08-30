81ed1dfdece2c62de7e23d9067a72bd5a96570a4
cubreporter51 <cubreporter51@comcast.net>
2019-08-30 10:16:15 -0400

    Creating file retrieval

---
+++ data.js
@@ -0,0 +1,18 @@
+[
+ {
+   "Name": "Elijah Miller",
+   "Code": "ejm"
+ },
+ {
+   "Name": "Lucas Miller",
+   "Code": "lgm"
+ },
+ {
+   "Name": "Garrett Dunmire",
+   "Code": "gld"
+ },
+ {
+   "Name": "Hannah Dunmire",
+   "Code": "had"
+ }
+]
